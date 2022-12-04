239b8eaa2091019420d654618ce634a24dc44884
Steinar <56594043+SteinarSi@users.noreply.github.com>
2022-12-04 19:27:37 +0100

    day 4

---
+++ AoC 2022/day4.hs
@@ -0,0 +1,14 @@
+import Data.List.Split (splitOn)
+import Data.List (intersect)
+import Data.Bool (bool)
+
+main :: IO ()
+main = do
+    inn <- fmap (map (map (map read . splitOn "-") . splitOn ",") . lines) $ readFile "day4-input.txt"
+    print (sum $ map fullOverlap inn, sum $ map overlap inn)
+
+overlap :: [[Int]] -> Int
+overlap [[a, b], [c, d]] = bool 1 0 (null $ intersect [a..b] [c..d])
+
+fullOverlap :: [[Int]] -> Int
+fullOverlap [[a, b], [c, d]] = bool 0 1 (elem (intersect [a..b] [c..d]) [[a..b], [c..d]])
