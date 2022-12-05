e978b1b6c538705142fa341a2926feaa0fbfce0a
SteinarSi <steinar.simonnes@gmail.com>
2022-12-05 16:52:03 +0100

    Liten refaktor.

--- AoC 2022/day1.hs
+++ AoC 2022/day1.hs
@@ -2,9 +2,4 @@ import Data.List.Split (splitWhen)
 import Data.List (sort)
 
 main :: IO ()
-main = readFile "day1-input.txt" >>= print .  foldr (\x xs -> max (sum $ map read x) xs) 0 . splitWhen null . lines
-
-main2 :: IO ()
-main2 = readFile "day1-input.txt" >>= print . sum . take 3 . reverse . sort . map (sum . map read) . splitWhen null . lines
-
-
+main = fmap (reverse . sort . map (sum . map read) . splitWhen null . lines) (readFile "day1-input.txt") >>= \(x:y:z:_) -> print (x, x+y+z)
