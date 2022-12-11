3f1c3de941bb2f7f420d7084fae2ca7e6f14c9f2
SteinarSi <steinar.simonnes@gmail.com>
2022-12-11 19:14:32 +0100

    AoC day 11 part 2

--- AoC 2022/day11.hs
+++ AoC 2022/day11.hs
@@ -6,53 +6,42 @@ import Prelude hiding (round)
 import Data.Function ((&))
 import Data.List (sort)
 
-
-
 main :: IO ()
 main = do
     inn <- fmap (map (map words) . splitWhen null . lines) (readFile "day11-input.txt")
-    let 
-        (ms1, is) = parse [] [] True  inn
+    let (ms1, is) = parse [] [] True  inn
         (ms2, _)  = parse [] [] False inn
-    print (simulate 20 is ms1) -- , simulate 10000 is ms2)
-
+    print (simulate 20 is ms1 , simulate 10000 is ms2)
 
 data Monkey = Monkey {
         name :: Int,
         operation :: Integer -> Integer,
         test :: Integer -> Bool,
+        divisor :: Integer,
         friend1 :: Int,
         friend2 :: Int
     }
 
-instance Show Monkey where
-    show m = '\n' : show (name m) ++ "\n" ++ show (test m 20) ++ "\n" ++ show (friend1 m) ++ "," ++ show (friend2 m) ++ "\n"
-
 simulate :: Int -> [[Integer]] -> [Monkey] -> Integer
 simulate n items ms = product $ take 2 $ reverse $ sort bs
-    where (bs, _) = foldl (&) (replicate (length ms) 0,items) (replicate n (`round` ms))
+    where (bs, _) = foldl (&) (replicate (length ms) 0,items) (replicate n (\is -> round (product (map divisor ms)) is ms))
 
-round :: ([Integer], [[Integer]]) -> [Monkey] -> ([Integer], [[Integer]])
-round = foldl (\(ns, is) m -> (modify (name m) (+fromIntegral (length (is!!name m))) ns, throw m (is !! name m) (modify (name m) (const []) is)))
+round :: Integer -> ([Integer], [[Integer]]) -> [Monkey] -> ([Integer], [[Integer]])
+round f = foldl (\(ns, is) m -> (modify (name m) (+fromIntegral (length (is!!name m))) ns, throw f m (is !! name m) (modify (name m) (const []) is)))
 
-throw :: Monkey -> [Integer] -> [[Integer]] -> [[Integer]]
-throw m []     items = items
-throw m (x:xs) items = throw m xs (modify (bool (friend2 m) (friend1 m) (test m x')) (x':) items)
-    where x' = operation m x
+throw :: Integer -> Monkey -> [Integer] -> [[Integer]] -> [[Integer]]
+throw f m []     items = items
+throw f m (x:xs) items = throw f m xs (modify (bool (friend2 m) (friend1 m) (test m x')) (x':) items)
+    where x' = operation m x `mod` f
 
 parse :: [Monkey] -> [[Integer]] -> Bool -> [[[String]]] -> ([Monkey], [[Integer]])
 parse ms items dv [] = (reverse ms, reverse items)
-parse ms items dv ([[monkey, n], 
-        (starting:items':is), 
-        (operation:new:equals:old:op), 
-        [test,divisible,by,d], 
-        [iff,true,throw,to,monkey',i], 
-        [iff',false,throw',to',monkey'',j]
-       ]:xs) = parse (m:ms) (read ('[' : concat is ++ "]") : items) dv xs
+parse ms items dv ([[_, n], (_:_:is), (_:_:_:_:op), [_,_,_,d], [_,_,_,_,_,i], [_,_,_,_,_,j]]:xs) = parse (m:ms) (read ('[' : concat is ++ "]") : items) dv xs
     where m = Monkey {
         name = read (init n),
         operation = parseOperation dv op,
         test = (0==) . (`mod` (read d)),
+        divisor = read d,
         friend1 = read i,
         friend2 = read j
     }
