eb2308d3fc3e75dc7982f42f7476d4004d745913
Steinar <56594043+SteinarSi@users.noreply.github.com>
2022-12-16 21:19:37 +0100

    AoC day 15 part 2

--- AoC 2022/day15.hs
+++ AoC 2022/day15.hs
@@ -1,35 +1,67 @@
-{-# LANGUAGE Strict #-}
+{-# LANGUAGE Strict, TupleSections #-}
 
 import Data.List (genericLength)
+import qualified Data.Set as S
+
+import Control.Concurrent
+import Control.DeepSeq (deepseq)
+import Control.Monad (forM_)
+import Control.Concurrent.QSemN
+
+
+import Debug.Trace (trace)
 
 target = 2000000
 
 main :: IO ()
 main = do
-    sensors <- fmap (map parse . map words . lines) (readFile "day15-input.txt")
-    let closeSensors = filter (\(Sensor (_,sy) _ d) -> abs (sy - target) <= d) sensors
-        max' = maximum $ map (\(Sensor (sx,_) _ d) -> sx+d) sensors
-        min' = minimum $ map (\(Sensor (sx,_) _ d) -> sx-d) sensors
-    print (genericLength $ filter (canHaveBeacon closeSensors . (,target)) [min'..max'])
+    getNumCapabilities >>= setNumCapabilities
+    beacons <- fmap (map parse . map words . lines) (readFile "day15-input.txt")
+    let closeBeacons = filter (\(Beacon (_,sy) _ d) -> abs (sy - target) <= d) beacons
+        max' = maximum $ map (\(Beacon (sx,_) _ d) -> sx+d) closeBeacons
+        min' = minimum $ map (\(Beacon (sx,_) _ d) -> sx-d) closeBeacons
+    print (genericLength $ filter (canHaveBeacon closeBeacons . (,target)) [min'..max'])
+    concurrentSearch beacons
 
+concurrentSearch :: [Beacon] -> IO ()
+concurrentSearch beacons = do
+    qsem <- newQSemN 0
+    forM_ beacons search
+    waitQSemN qsem (length beacons)
+    where search beacon = let signals = findSignal beacon beacons
+                          in  if null (signals)
+                            then pure ()
+                            else print (map (\(x, y) -> (x,y, 4000000*x+y)) signals)
 
-
-data Sensor = Sensor (Int, Int) (Int, Int) Int
+data Beacon = Beacon {
+        beacon :: (Int, Int),
+        sensor :: (Int, Int), 
+        distance :: Int
+    }
     deriving (Show)
 
-canHaveBeacon :: [Sensor] -> (Int, Int) -> Bool
+edges :: Beacon -> S.Set (Int, Int)
+edges (Beacon (bx,by) ~_ d) = S.fromList $ filter (\(x,y) -> x>=0 && x<=4000000 && y>=0 && y<=4000000) allEdges
+    where allEdges = concat [ [(bx+d+1-i, by+i), (bx+i, by-d-1+i), (bx-d-1+i, by-i), (bx-i, by+d+1-i)] | i <- [0..d-1] ]
+
+findSignal :: Beacon -> [Beacon] -> [(Int, Int)]
+findSignal source ss = (S.toList $ S.filter (\e -> all (\tc -> manhattan e (beacon tc) > distance tc) toCheck) $ edges source)
+    where toCheck = filter (\b -> manhattan (beacon source) (beacon b) <= distance source + distance b) ss
+
+canHaveBeacon :: [Beacon] -> (Int, Int) -> Bool
 canHaveBeacon [] _ = False
-canHaveBeacon (Sensor s@(sx,sy) b@(bx,by) d :xs) p@(x,y) | b == p = False
+canHaveBeacon (Beacon s@(sx,sy) b@(bx,by) d :xs) p@(x,y) | b == p = False
                                                          | manhattan s p <= d = True
                                                          | otherwise = canHaveBeacon xs p
 
 manhattan :: (Int, Int) -> (Int, Int) -> Int
 manhattan (a,b) (c,d) = abs (a-c) + abs (b-d)
 
-parse :: [String] -> Sensor
-parse (_:_:x:y:_:_:_:_:x':y':_) = Sensor (s1, s2) (b1, b2) (manhattan (s1,s2) (b1,b2))
+parse :: [String] -> Beacon
+parse (_:_:x:y:_:_:_:_:x':y':_) = Beacon (s1, s2) (b1, b2) (manhattan (s1,s2) (b1,b2))
     where d = read . drop 2 . init
           s1 = d x
           s2 = d y
           b1 = d x'
           b2 = read (drop 2 y')
+
