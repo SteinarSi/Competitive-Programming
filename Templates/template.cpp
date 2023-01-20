033b9440aa3b701779471271ef371020b457c747
SteinarSi <steinar.simonnes@gmail.com>
2023-01-20 12:41:54 +0100

    Template for cpp

---
+++ Templates/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+using i64 = int64_t;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.exceptions(ios::failbit);
+    cout << setprecision(10) << fixed;
+}
+
