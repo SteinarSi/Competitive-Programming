90fe406f03917b1edff444d1ed105a2e772456bb
Steinar <steinar.simonnes@gmail.com>
2023-02-28 16:38:01 +0100

    INF237 A Multiplication Game, redone

--- Kattis/INF237/Uke4/AMultiplicationGame.cpp
+++ Kattis/INF237/Uke4/AMultiplicationGame.cpp
@@ -3,40 +3,37 @@
 
 using namespace std;
 
-int maxi(long p, map<long, int> &memo, long n);
-int mini(long p, map<long, int> &memo, long n);
+int maxi(long p, long n, map<long, bool> &memo);
+int mini(long p, long n, map<long, bool> &memo);
 
-int maxi(long p, map<long, int> &memo, long n){
-    if (p * 9 >= n) return 1;
-    if (memo.find(p) != memo.end()) return memo[p];
-    int ret = mini(p*9, memo, n) == 1 || mini(p*2, memo, n) == 1 ? 1 : -1;
-    memo[p] = ret;
-    return ret;
-}
-
-int mini(long p, map<long, int> &memo, long n){
-    if (p * 9 >= n) return -1;
-    if (memo.find(p) != memo.end()) return memo[p];
-    int ret = maxi(p*9, memo, n) == -1 || maxi(p*2, memo, n) == -1 ? -1 : 1;
-    memo[p] = ret;
-    return ret;
-}
-
-void simon(long n){
-    long p {1};
-    bool player {false};
-    while (p < n){
-        player = ! player;
-        if (player) p *= 9;
-        else p *= 2;
+int maxi(long p, long n, map<long, bool> &memo){
+    if (p * 9 >= n || (memo.count(p) && memo[p])) return 1;
+    int r = -1;
+    for (int i {2}; i <= 9; i++){
+        r = max(r, mini(p*i, n, memo));
+        if (r == 1) break;
     }
-    if (player) printf("Stan wins.\n");
-    else printf("Ollie wins.\n");
+    memo[p] = r == 1;
+    return r;
+}
+
+int mini(long p, long n, map<long, bool> &memo){
+    if (p * 9 >= n || (memo.count(p) && memo[p])) return -1;
+    int r = 1;
+    for (int i {2}; i <= 9; i++){
+        r = min(r, maxi(p*i, n, memo));
+        if (r == -1) break;
+    }
+    memo[p] = r == -1;
+    return r;
 }
 
 int main(){
     long n;
+    map<long, bool> memo;
     while (cin >> n){
-        simon(n);
+        memo = map<long, bool>();
+        if (maxi(1, n, memo) == 1) printf("Stan wins.\n");
+        else printf("Ollie wins.\n");
     }
 }
