77e8d1be0bd277dcbda1c09be932ba9cc5ecd0af
Steinar <steinar.simonnes@gmail.com>
2023-03-20 23:20:26 +0100

    Eye of Sauron

---
+++ Kattis/EyeOfSauron.hs
@@ -0,0 +1,7 @@
+main = do
+    inn <- getLine
+    let b = takeWhile (=='|') inn
+    let a = takeWhile (=='|') $ drop (length b + 2) inn
+    if a == b 
+        then putStrLn "correct"
+        else putStrLn "fix"
