b0a3d097740561f21eac116fea4f67adea5bd755
Steinar <steinar.simonnes@gmail.com>
2023-05-22 20:40:36 +0200

    Make Divisible Subsequences even shorter

--- Kattis/INF237/Uke3/DivisibleSubsequences.py
+++ Kattis/INF237/Uke3/DivisibleSubsequences.py
@@ -1,4 +1,5 @@
-for _ in range(int(input())):
- d=int(input().split()[0]);s,m=0,[0]*d
- for i in input().split():s+=int(i);m[s%d]+=1
+I=input
+for _ in range(int(I())):
+ d=int(I().split()[0]);s,m=0,[0]*d
+ for i in I().split():s+=int(i);m[s%d]+=1
  print(m[0]*(m[0]+1)//2+sum(n*(n-1)//2 for n in m[1:]))
