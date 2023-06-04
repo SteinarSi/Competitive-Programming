a1e25ac99b999d78ccab41237fed8fceecce0c0b
Steinar <steinar.simonnes@gmail.com>
2023-06-04 11:45:50 +0200

    Update Bridges and Tunnels to use the ID class instead

--- Kattis/INF237/Uke5/BridgesAndTunnels.cpp
+++ Kattis/INF237/Uke5/BridgesAndTunnels.cpp
@@ -3,33 +3,34 @@
 #include <map>
 
 using namespace std;
+using i64 = int64_t;
 
 class UF {
-    vector<long long> repr;
-    vector<long long> size;
+    vector<i64> repr;
+    vector<i64> size;
 
     public:
-    UF(long long n){
-        repr = vector<long long>(n, 0);
-        size = vector<long long>(n, 1);
-        for (long long i {0}; i < n; i++){
+    UF(i64 n){
+        repr = vector<i64>(n, 0);
+        size = vector<i64>(n, 1);
+        for (i64 i {0}; i < n; i++){
             repr[i] = i;
         }
     }
 
-    long long find(long long u){
+    i64 find(i64 u){
         if (repr[u] == u) return u;
-        long long r = find(repr[u]);
+        i64 r = find(repr[u]);
         repr[u] = r;
         return r;
     }
 
-    void merge(long long u, long long v){
-        long long r1 = find(u);
-        long long r2 = find(v);
+    void merge(i64 u, i64 v){
+        i64 r1 = find(u);
+        i64 r2 = find(v);
         if (r1 == r2) return;
-        long long s1 = size[r1];
-        long long s2 = size[r2];
+        i64 s1 = size[r1];
+        i64 s2 = size[r2];
         if (s1 < s2){
             repr[r1] = r2;
             size[r2] += size[r1];
@@ -40,30 +41,40 @@ class UF {
         }
     }
 
-    long long size_of(long long u){
+    i64 size_of(i64 u){
         return size[find(u)];
+    }
+};
+
+class ID {
+    map<string,i64> name2id;
+    vector<string> id2name;
+
+public:
+    void add(const string &s){
+        get_id(s);
+    }
+
+    i64 get_id(const string &s){
+        auto [it, placed] = name2id.emplace(s, name2id.size());
+        if (placed) id2name.push_back(s);
+        return it->second;
     }
 };
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    long long n;
+    i64 n;
     cin >> n;
     UF uf = UF(200000);
-    map<string, long long> name_to_id;
-    vector<string> id_to_name;
-    auto get_id = [&](const string &s) {
-        auto [it, placed] = name_to_id.emplace(s, name_to_id.size());
-        if (placed) id_to_name.push_back(s);
-        return it->second;
-    };
+    ID id = ID();
     string a, b;
-    long long f, t;
-    for (long long i {0}; i < n; i++){
+    i64 f, t;
+    for (i64 i {0}; i < n; i++){
         cin >> a >> b;
-        f = get_id(a);
-        t = get_id(b);
+        f = id.get_id(a);
+        t = id.get_id(b);
         uf.merge(f, t);
         cout << uf.size_of(f) << endl;
     }
