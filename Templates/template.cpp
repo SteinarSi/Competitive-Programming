3295c4ae88ae2479985a5a74d285f29038f8cf73
Steinar <steinar.simonnes@gmail.com>
2023-06-04 11:54:52 +0200

    Update template for C++

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <map>
+#include <queue>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 using i64 = int64_t;
+using u64 = uint64_t;
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cin.exceptions(ios::failbit);
     cout << setprecision(10) << fixed;
-
-
-    map<string, long long> name_to_id;
-    vector<string> id_to_name;
-    auto get_id = [&](const string &s) {
-        auto [it, placed] = name_to_id.emplace(s, name_to_id.size());
-        if (placed) id_to_name.push_back(s);
-        return it->second;
-    };
-
 }
-
