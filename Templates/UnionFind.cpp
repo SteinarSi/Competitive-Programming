c5c92f0ad083407b2aa8501954b355773ffe643e
Steinar <steinar.simonnes@gmail.com>
2023-06-04 12:14:30 +0200

    Update Union Find

--- Templates/UnionFind.cpp
+++ Templates/UnionFind.cpp
@@ -2,32 +2,31 @@
 
 using namespace std;
 
+template <typename Num>
 class UF {
-    vector<long long> repr;
-    vector<long long> size;
+    vector<Num> repr;
+    vector<Num> size;
+    Num comps;
 
     public:
-    UF(long long n){
-        repr = vector<long long>(n, 0);
-        size = vector<long long>(n, 1);
-        for (long long i {0}; i < n; i++){
-            repr[i] = i;
-        }
+    UF(Num n){
+        repr = vector<Num>(n, 0);
+        size = vector<Num>(n, 1);
+        comps = n;
+        for (Num i {0}; i < n; repr[i++] = i);
     }
-
-    long long find(long long u){
+    Num find(Num u){
         if (repr[u] == u) return u;
-        long long r = find(repr[u]);
+        Num r = find(repr[u]);
         repr[u] = r;
         return r;
     }
-
-    void merge(long long u, long long v){
-        long long r1 = find(u);
-        long long r2 = find(v);
+    void merge(Num u, Num v){
+        Num r1 = find(u);
+        Num r2 = find(v);
         if (r1 == r2) return;
-        long long s1 = size[r1];
-        long long s2 = size[r2];
+        Num s1 = size[r1];
+        Num s2 = size[r2];
         if (s1 < s2){
             repr[r1] = r2;
             size[r2] += size[r1];
@@ -36,9 +35,8 @@ class UF {
             repr[r2] = r1;
             size[r1] += size[r2];
         }
+        comps--;
     }
-
-    long long size_of(long long u){
-        return size[find(u)];
-    }
+    Num size_of(Num u){ return size[find(u)]; }
+    Num components(){ return comps; }
 };
