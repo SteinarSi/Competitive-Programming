50b5a0f20b515483ef9e1746833dc07cba84127b
Steinar <steinar.simonnes@gmail.com>
2023-09-25 13:58:39 +0200

    The Easiest Problem Is This One

---
+++ Kattis/TheEasiestProblemIsThisOne.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int digit_sum(int p) {
+    if (p == 0) return 0;
+    return p % 10 + digit_sum(p / 10);
+}
+
+int solve(int p) {
+    int s = digit_sum(p);
+    int q = 11;
+    while (true) {
+        if (digit_sum(p * q) == s) return q;
+        q++;
+    }
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.exceptions(ios::failbit);
+    int p;
+    while (cin >> p && p > 0){
+        cout << solve(p) << '\n';
+    }
+}
