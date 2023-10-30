22812c1c938a8ba3a389c9a11e8b6363b5ad211e
Steinar <steinar.simonnes@gmail.com>
2023-10-30 13:19:40 +0100

    Fix up the cpp Template

---
+++ Templates/Template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <map>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <cstdint>
+
+using namespace std;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.exceptions(ios::failbit);
+    cout << setprecision(10) << fixed;
+}
