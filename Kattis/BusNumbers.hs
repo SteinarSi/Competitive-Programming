32f4dc3ef10e07f71b4fd5922a8d2f9178bd5a1a
Steinar <steinar.simonnes@gmail.com>
2023-11-04 12:59:19 +0100

    Bus Numbers

---
+++ Kattis/BusNumbers.hs
@@ -0,0 +1,15 @@
+import Data.List (sort)
+
+main :: IO ()
+main = interact (unwords . map (format . map show) . group [] . sort . map read . tail . words) >> putChar '\n'
+
+format :: [String] -> String
+format xs | length xs >= 3 = head xs ++ "-" ++ last xs
+          | otherwise      = unwords xs
+
+group :: [Int] -> [Int] -> [[Int]]
+group [] [] = []
+group xs [] = [reverse xs]
+group [] (x:xs) = group [x] xs
+group (y:ys) (x:xs) | x == succ y = group (x:y:ys) xs
+                    | otherwise   = reverse (y:ys) : group [x] xs
