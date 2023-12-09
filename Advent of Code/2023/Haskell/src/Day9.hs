232f0e325aa05fc84d736415bb0fb4f67deb0885
Steinar <steinar.simonnes@gmail.com>
2023-12-09 16:19:22 +0100

    AoC Day 9

---
+++ Advent of Code/2023/Haskell/src/Day9.hs
@@ -0,0 +1,18 @@
+module Day9 where
+
+import           Meta (AoC (..))
+
+data Day9 = Day9
+instance AoC Day9 [[Int]] Int where
+    parse _ = map (map read . words) . lines
+    part1 _ = predicc
+    part2 _ = predicc . map reverse
+    date _ = 9
+    testAnswerPart1 _ = 114
+    testAnswerPart2 _ = 2
+
+predicc :: [[Int]] -> Int
+predicc = sum . map (sum . map last . takeWhile (any (/=0)) . iterate diffs)
+
+diffs :: [Int] -> [Int]
+diffs xs = zipWith subtract xs (tail xs)
--- Advent of Code/2023/Haskell/src/Meta.hs
+++ Advent of Code/2023/Haskell/src/Meta.hs
@@ -1,11 +1,12 @@
 module Meta (test, solve, AoC(..), bench, benchAll) where
 
-import Control.Monad (when, void, forM)
-import Prelude hiding (log)
+import           Control.Monad   (forM, void, when)
+import           Prelude         hiding (log)
 
-import Data.Time.Clock (diffUTCTime, getCurrentTime, NominalDiffTime, nominalDiffTimeToSeconds)
-import Text.Printf (printf)
-import Data.Fixed (showFixed)
+import           Data.Fixed      (showFixed)
+import           Data.Time.Clock (NominalDiffTime, diffUTCTime, getCurrentTime,
+                                  nominalDiffTimeToSeconds)
+import           Text.Printf     (printf)
 
 class (Eq answer, Show answer) => AoC day problem answer | day -> problem answer where
     parse :: day -> String  -> problem
@@ -20,7 +21,7 @@ test day = do
     putStrLn $ "Testing Day " ++ show (date day) ++ ": "
     (s1, s2) <- meta day ("inputs/day" ++ show (date day) ++ "-test.txt")
     if s1 /= testAnswerPart1 day
-        then putStrLn $ "    Got wrong answer on part 1: " ++ show s1 ++ " /= " ++ show (testAnswerPart1 day) 
+        then putStrLn $ "    Got wrong answer on part 1: " ++ show s1 ++ " /= " ++ show (testAnswerPart1 day)
         else putStrLn   "    Part 1 is correct!"
     if s2 /= testAnswerPart2 day
         then putStrLn $ "    Got wrong answer on part 2: " ++ show s2 ++ " /= " ++ show (testAnswerPart2 day)
@@ -48,21 +49,20 @@ benchmark log action = do
     void action
     end    <- getCurrentTime
     let time = diffUTCTime end start
-    when log $ putStrLn ("\nTime spent: " ++ formatTime time)
+    when log $ putStrLn ("Time spent: " ++ formatTime time)
     pure time
 
 benchAll :: Bool -> [IO a] -> IO NominalDiffTime
 benchAll log actions = do
     dash
     total <- fmap sum $ forM actions $ \action -> do
-        putChar '\n'
         time <- benchmark log action
         dash
         pure time
-    putStrLn ("\nTime spent in total: " ++ formatTime total)
+    putStrLn ("Time spent in total: " ++ formatTime total)
     pure total
 
-    where dash = putStrLn ('\n' : replicate 100 '=')
+    where dash = putStrLn (replicate 50 '=')
 
 formatTime :: NominalDiffTime -> String
 formatTime = printf "%.4fs" . (read :: String -> Double) . showFixed True . nominalDiffTimeToSeconds
