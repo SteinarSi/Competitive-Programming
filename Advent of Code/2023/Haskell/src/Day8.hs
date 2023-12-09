13f3b46376dc6bb64c60b021719e0130c2f51a43
Steinar <steinar.simonnes@gmail.com>
2023-12-09 16:08:22 +0100

    AoC Day 8

---
+++ Advent of Code/2023/Haskell/src/Day8.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module Day8 where
+
+import           Data.Char  (isAlphaNum)
+import           Data.Map   (Map, fromList, keys, lookup)
+import           Data.Maybe (fromJust)
+import           Prelude    hiding (lookup)
+
+import           Meta       (AoC (..))
+
+data Day8 = Day8
+instance AoC Day8 (String, Graph) Int where
+    parse _ s = (dirs, fromList (map parseRow rest))
+        where (dirs:_:rest) = lines s
+    part1 _ (dirs, graph) = follow graph (cycle dirs) "AAA"
+    part2 _ (dirs, graph) = foldr (lcm . follow graph (cycle dirs)) 1 . filter (\s -> last s == 'A')  $ keys graph
+    date _ = 8
+    testAnswerPart1 _ = 2
+    testAnswerPart2 _ = 2
+
+type Graph = Map String (String, String)
+
+parseRow :: String -> (String, (String, String))
+parseRow s = (from, (l, r))
+    where (from:_:l:r:_) = map (filter isAlphaNum) $ words s
+
+follow :: Graph -> String -> String -> Int
+follow graph (d:dirs) u | last u == 'Z' = 0
+                        | otherwise     = succ . follow graph dirs . leftRight . fromJust $ lookup u graph
+    where leftRight | d == 'L'  = fst
+                    | otherwise = snd
--- Advent of Code/2023/Haskell/src/Utils.hs
+++ Advent of Code/2023/Haskell/src/Utils.hs
@@ -1,9 +1,12 @@
-module Utils (takeDropWhile, padWith, toArray, mapSome, toTuple, toTriple, read') where
+module Utils (takeDropWhile, padWith, toArray, mapSome, toTuple, toTriple, read', length', trace', revSort) where
 
-import Data.Array (Array, array)
-import Data.Bifunctor (first)
-import Data.Maybe (fromMaybe)
-import Text.Read (readMaybe)
+import           Data.Array     (Array, array)
+import           Data.Bifunctor (first)
+import           Data.List      (sortBy)
+import           Data.Maybe     (fromMaybe)
+import           Data.Ord       (Down (Down), comparing)
+import           Debug.Trace    (trace)
+import           Text.Read      (readMaybe)
 
 
 takeDropWhile :: (a -> Bool) -> [a] -> ([a], [a])
@@ -21,17 +24,26 @@ toArray xss = array b l
           b = ((0, 0), (length (head xss)-1, length xss-1))
 
 mapSome :: Integral i => (a -> a) -> i -> [a] -> [a]
-mapSome _ 0 xs = xs
-mapSome _ _ [] = []
+mapSome _ 0 xs     = xs
+mapSome _ _ []     = []
 mapSome f i (x:xs) = f x : mapSome f (pred i) xs
 
 toTuple :: Show a => [a] -> (a, a)
 toTuple [a, b] = (a, b)
-toTuple xs = error ("List doesn't have two elements: " ++ show xs)
+toTuple xs     = error ("List doesn't have two elements: " ++ show xs)
 
 toTriple :: Show a => [a] -> (a, a, a)
 toTriple [a, b, c] = (a, b, c)
-toTriple xs = error ("List doesn't have three elements: " ++ show xs)
+toTriple xs        = error ("List doesn't have three elements: " ++ show xs)
+
+revSort :: Ord a => [a] -> [a]
+revSort = sortBy (comparing Down)
 
 read' :: (Read r, Show r) => String -> r
 read' s = fromMaybe (error ("Could not read " ++ s)) (readMaybe s)
+
+length' :: Integral i => [a] -> i
+length' = fromIntegral . length
+
+trace' :: Show s => s -> a -> a
+trace' s = trace (show s)
