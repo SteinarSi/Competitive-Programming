88c06b0b8e91549e9a8fcead69e8836083da07e2
Steinar <steinar.simonnes@gmail.com>
2024-01-22 10:40:06 +0100

    Rewrite GCD to implement Euclid's manually

--- Kattis/GCD.hs
+++ Kattis/GCD.hs
@@ -1,4 +1,10 @@
+import           Prelude hiding (gcd)
+
 main :: IO ()
 main = do
     [a, b] <- fmap (map read . words) getLine
     print (gcd a b)
+
+gcd :: Integral i => i -> i -> i
+gcd a 0 = a
+gcd a b = gcd b (a `mod` b)
