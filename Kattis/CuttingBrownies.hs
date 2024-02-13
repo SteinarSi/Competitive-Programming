2007c4edbf29e34fe23ff09f53c454430c8569a2
Steinar <steinar.simonnes@gmail.com>
2024-02-13 11:18:09 +0100

    Cutting Brownies

---
+++ Kattis/CuttingBrownies.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Arrow         ((>>>))
+import qualified Data.ByteString.Char8 as C
+import           Data.Function         (on)
+import           Data.Maybe            (fromJust)
+
+data Player = Harry | Vicky
+    deriving (Read, Eq)
+
+main :: IO ()
+main = C.getContents >>= (
+            C.lines
+        >>> tail
+        >>> map (C.words
+            >>> (\[breadth,depth,player] -> (readInt breadth, readInt depth, player))
+            >>> solve
+        )
+        >>> C.unlines
+        >>> C.putStr
+    )
+
+solve :: (Int,Int,C.ByteString) -> C.ByteString
+solve (b, d, p) | on (>) (fromIntegral >>> logBase 2 >>> floor) x y = p <> " can win"
+                | otherwise    = p <> " cannot win"
+    where (x,y) | p == "Harry" = (d, b)
+                | otherwise    = (b, d)
+
+readInt :: C.ByteString -> Int
+readInt = C.readInt >>> fromJust >>> fst
