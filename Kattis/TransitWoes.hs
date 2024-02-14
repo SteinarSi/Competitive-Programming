0413aa4fa138f9ff119fb5381b59c24abb1fd064
Steinar <steinar.simonnes@gmail.com>
2024-02-14 15:39:37 +0100

    Transit Woes

---
+++ Kattis/TransitWoes.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Arrow         ((>>>))
+import           Control.Monad         (ap)
+import qualified Data.ByteString.Char8 as C
+import           Data.Maybe            (fromJust)
+
+import           Debug.Trace           (traceShow)
+
+main :: IO ()
+main = do
+    [s, t, n] <- readInts
+    w:walks <- readInts
+    rides <- readInts
+    intervals <- readInts
+    C.putStrLn $ if solve (s+w) (zip3 walks rides intervals) <= t
+        then "yes"
+        else "no"
+
+solve :: Int -> [(Int,Int,Int)] -> Int
+solve time [] = time
+solve time ((walk,ride,interval):xs) = solve (time + (interval - time `mod` interval) `mod` interval + ride + walk) xs
+
+readInts :: IO [Int]
+readInts = fmap (C.words >>> map (C.readInt >>> fromJust >>> fst)) C.getLine
