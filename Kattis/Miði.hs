fbeda1643667af0fc90242f625d079f435197c69
Steinar <steinar.simonnes@gmail.com>
2024-04-15 12:31:30 +0200

    Miði

---
+++ Kattis/Miði.hs
@@ -0,0 +1,12 @@
+import           Control.Arrow         ((>>>))
+import qualified Data.ByteString.Char8 as C
+
+main :: IO ()
+main = C.getContents >>= (
+            C.lines
+        >>> tail
+        >>> map C.reverse
+        >>> reverse
+        >>> C.concat
+        >>> C.putStrLn
+    )
