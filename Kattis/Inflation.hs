ccb48ab32afd279e72cf20a111c39fdd3c1d96dd
Steinar <steinar.simonnes@gmail.com>
2024-11-17 22:19:52 +0100

    Format Inflation

--- Kattis/Inflation.hs
+++ Kattis/Inflation.hs
@@ -1,4 +1,4 @@
-import Data.List (sort)
+import           Data.List (sort)
 
 main :: IO ()
 main = do
@@ -6,7 +6,7 @@ main = do
     kanner <- getLine
     case smallest (zip [1..read input] (sort [read x | x <- words kanner])) of
         Nothing -> putStrLn "impossible"
-        Just x -> print x
+        Just x  -> print x
 
 smallest :: [(Float, Float)] -> Maybe Float
 smallest [] = Just 1
@@ -14,4 +14,4 @@ smallest ((b, k):xs)
     | k/b > 1 = Nothing
     | otherwise = case smallest xs of
         Nothing -> Nothing
-        Just x -> Just (min (k/b) x)
+        Just x  -> Just (min (k/b) x)
