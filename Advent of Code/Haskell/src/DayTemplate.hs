f9e71bbd64f85c857eb949bd2407d01cdd41f292
Steinar <steinar.simonnes@gmail.com>
2024-12-07 21:40:23 +0100

    Add AoC template

---
+++ Advent of Code/Haskell/src/DayTemplate.hs
@@ -0,0 +1,17 @@
+module Year.Year2024.DayX (DayX(DayX)) where
+
+-- This module only exists as a template to new solutions.
+
+import           Meta  (AoC (..))
+import           Utility.Misc
+
+data DayX = DayX
+instance AoC DayX [String] Int where
+    parse _ = lines
+    part1 _ = const 0
+    part2 _ = const 0
+    date _  = -1
+    year _  = 2024
+    testAnswerPart1 _ = -1
+    testAnswerPart2 _ = -1
+    debug _ ps = []
