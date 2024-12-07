097a426d5a28779d660f725345388dc44b2e7426
Steinar <steinar.simonnes@gmail.com>
2024-12-07 21:15:51 +0100

    Updating AoC year 2023 day 3

--- Advent of Code/Haskell/src/Year/Year2023/Day3.hs
+++ Advent of Code/Haskell/src/Year/Year2023/Day3.hs
@@ -1,31 +1,40 @@
-module Year.Year2023.Day3 where
+module Year.Year2023.Day3 (Day3(Day3)) where
 
-import           Data.Array     (Array, (!))
-import           Data.Bifunctor (bimap)
-import           Data.Char      (isControl, isDigit)
-import           Data.List      (groupBy, sort)
+import           Control.Arrow   ((>>>), (***))
+import           Data.Array.Base (UArray, (!))
+import           Data.Char       (isControl, isDigit)
+import           Data.Function   ((&))
+import           Data.List       (groupBy, sort)
 
-import           Meta           (AoC (..))
-import           Utility.Misc   (padWith, takeDropWhile, toArray)
+import           Meta            (AoC (..))
+import           Utility.Misc    (padWith, takeDropWhile, toArray)
 
 data Day3 = Day3
 instance AoC Day3 ([((Int, Int), Int)], [Int]) Int where
     parse _ input = search 1 1 (toArray (padWith '.' l)) l
         where l = lines input
-    part1 _ = sum  . snd
-    part2 _ = sum . map product . filter ((2==) . length) . map (map snd) . groupBy (\a b -> fst a == fst b) . sort . fst
+    part1 _ = snd >>> sum
+    part2 _ = fst
+            >>> sort
+            >>> groupBy (\a b -> fst a == fst b)
+            >>> map (map snd)
+            >>> filter (length >>> (2==))
+            >>> map product
+            >>> sum
     date _ = 3
     year _ = 2023
     testAnswerPart1 _ = 4361
     testAnswerPart2 _ = 467835
 
-search :: Int -> Int -> Array (Int, Int) Char -> [String] -> ([((Int, Int), Int)], [Int])
+search :: Int -> Int -> UArray (Int, Int) Char -> [String] -> ([((Int, Int), Int)], [Int])
 search _ _ _ [] = ([], [])
 search _ y arr ("":xss) = search 1 (succ y) arr xss
-search x y arr ((s:ss):xss) | isDigit s && any (isSymbol . (arr!)) neighbours = bimap (gears ++) (read dits :) $ search (x+length dits) y arr (rest : xss)
-                            | otherwise = search (succ x) y arr (ss:xss)
-    where (dits, rest) = takeDropWhile isDigit (s:ss)
-          neighbours   = [(h,v) | h <- [x-1..x + length dits], v <- [y-1..y+1]]
+search x y arr ((s:ss):xss) 
+        | isDigit s && any ((arr!) >>> isSymbol) neighbours = search (x+length dits) y arr (rest : xss)
+                                                                & (gears ++) *** (read dits :)
+        | otherwise                                         = search (succ x) y arr (ss:xss)
+    where (dits, rest) = span isDigit (s:ss)
+          neighbours   = [(v,h) | h <- [x-1..x + length dits], v <- [y-1..y+1]]
           gears        = [ (c, read dits)  | c <- neighbours, arr ! c == '*']
 
 isSymbol :: Char -> Bool
