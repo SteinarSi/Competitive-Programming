97554952dc62b17573160a9a6e30802062a8a110
Steinar <steinar.simonnes@gmail.com>
2024-12-07 20:53:31 +0100

    Updating AoC year 2023 day 23

--- Advent of Code/Haskell/src/Year/Year2023/Day23.hs
+++ Advent of Code/Haskell/src/Year/Year2023/Day23.hs
@@ -1,19 +1,19 @@
-module Year.Year2023.Day23(Day23(Day23)) where
+module Year.Year2023.Day23 (Day23(Day23)) where
 
-import           Data.Array      (Array)
-import           Data.Array.Base (bounds, indices, (!))
-import           Data.Bifunctor
-import           Data.Map        (Map, fromList, lookup)
-import           Data.Maybe      (fromJust, mapMaybe)
-import           Data.Set        (Set)
-import qualified Data.Set        as S
-import           Meta            (AoC (..))
-import           Prelude         hiding (lookup)
-import           Utility.Misc
+import           Control.Arrow             (second, (>>>))
+import           Data.Array.Base           (UArray, bounds, indices, (!))
+import           Data.Function             ((&))
+import           Data.List                 (find)
+import           Data.Maybe                (fromJust, mapMaybe)
+
+import           Meta                      (AoC (..))
+import           Utility.Misc              (directions, inBounds, supremum, toArray, (+++), trace')
+import qualified Utility.Structure.HashMap as HM
+import qualified Utility.Structure.HashSet as HS
 
 data Day23 = Day23
-instance AoC Day23 (Pos, Pos, Map Pos [(Int,Pos,Bool)]) Int where
-    parse _ = contract . toArray . lines
+instance AoC Day23 (Pos, Pos, HM.HashMap [(Int,Pos,Bool)]) Int where
+    parse _ = lines >>> toArray >>> contract
     part1 _ = longLongWaaaaaaalk False
     part2 _ = longLongWaaaaaaalk True
     date _ = 23
@@ -23,38 +23,46 @@ instance AoC Day23 (Pos, Pos, Map Pos [(Int,Pos,Bool)]) Int where
 
 type Pos = (Int,Int)
 
-contract :: Array Pos Char -> (Pos, Pos, Map Pos [(Int,Pos,Bool)])
-contract grid = (s, t, fromList [ (p, poiNeighbours p) | p <- poi ])
+contract :: UArray Pos Char -> (Pos, Pos, HM.HashMap [(Int,Pos,Bool)])
+contract grid = (s, t, HM.fromList [ (p, poiNeighbours p) | p <- poi ])
     where
-        s = (1,0)
-        t = first pred (snd (bounds grid))
+        s = (0,1)
+        t = second pred (snd (bounds grid))
         poi = s : t : filter isInteresting (indices grid)
-        poiNeighbours p = mapMaybe (reachable False 1 p) (neighs p)
 
+        poiNeighbours :: Pos -> [(Int, Pos, Bool)]
+        poiNeighbours p | p == t    = []
+                        | otherwise = mapMaybe (reachable False 1 p) (neighs p)
+
+        neighs :: Pos -> [Pos]
         neighs p = filter (\q -> inBounds grid q && grid ! q /= '#') $ map (p+++) directions
 
         reachable :: Bool -> Int -> Pos -> Pos -> Maybe (Int,Pos,Bool)
-        reachable climb cost prev curr | curr == s || curr == t || isInteresting curr = Just (cost,curr,climb)
-                                       | null next = Nothing
-                                       | otherwise = reachable climb' (cost+1) curr next
-            where nexts = filter (prev/=) (neighs curr)
-                  [next] = nexts
-                  c = grid ! curr
-                  climb' = climb || (
-                        c == '>' && next == curr +++ (-1,0) ||
-                        c == '<' && next == curr +++ (1,0) ||
-                        c == '^' && next == curr +++ (0,1) ||
-                        c == 'v' && next == curr +++ (0,-1)
-                    )
+        reachable climb cost prev curr 
+                | curr == s || curr == t || isInteresting curr = Just (cost,curr,climb)
+                | null next = Nothing
+                | otherwise = reachable climb' (cost+1) curr next
+            where 
+                next = neighs curr
+                    & find (prev/=)
+                    & fromJust
+                c = grid ! curr
+                climb' = climb || (
+                    c == '>' && next == curr +++ (0,-1) ||
+                    c == '<' && next == curr +++ (0,1) ||
+                    c == '^' && next == curr +++ (1,0) ||
+                    c == 'v' && next == curr +++ (-1,0))
 
         isInteresting :: Pos -> Bool
-        isInteresting p = length (neighs p) > 2
+        isInteresting p = grid ! p /= '#' && length (neighs p) > 2
 
-longLongWaaaaaaalk :: Bool -> (Pos, Pos, Map Pos [(Int,Pos,Bool)]) -> Int
-longLongWaaaaaaalk canClimb (start, end, graph) = dfs S.empty start
+longLongWaaaaaaalk :: Bool -> (Pos, Pos, HM.HashMap [(Int,Pos,Bool)]) -> Int
+longLongWaaaaaaalk canClimb (start, end, graph) = dfs HS.empty start
     where
-        dfs :: Set Pos -> Pos -> Int
+        dfs :: HS.HashSet -> Pos -> Int
         dfs visited u | u == end  = 0
-                      | otherwise = supremum $ map (\(cost, v, _) -> cost + dfs (S.insert u visited) v) next
-            where
-                next = filter (\(_,v,climb) -> (canClimb || not climb) && S.notMember v visited) (fromJust (lookup u graph))
+                      | otherwise = HM.lookup u graph
+                            & fromJust
+                            & filter (\(_,v,climb) -> (canClimb || not climb) && HS.notMember v visited)
+                            & map (\(cost, v, _) -> cost + dfs (HS.insert u visited) v)
+                            & supremum
