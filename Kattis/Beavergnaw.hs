6607956909ca8982b140b2ae16a75b7a0ede4f3a
Steinar <steinar.simonnes@gmail.com>
2025-01-31 17:51:36 +0100

    Reformat Beavergnaw

--- Kattis/Beavergnaw.hs
+++ Kattis/Beavergnaw.hs
@@ -1,4 +1,4 @@
-import           Control.Arrow         ((>>>), (&&&))
+import           Control.Arrow         ((&&&), (>>>))
 import qualified Data.ByteString.Char8 as C
 import           Data.Maybe            (fromJust)
 
