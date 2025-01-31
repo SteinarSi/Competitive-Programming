d698041b4451073becd60c29c0b02e55dbcfa4f9
Steinar <steinar.simonnes@gmail.com>
2025-01-31 17:50:41 +0100

    Deterministic Finite Automata - Is the Empty Language?

---
+++ Kattis/DeterministicFiniteAutomata-IsTheEmptyLanguage?.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Arrow         ((>>>), first)
+import           Control.Monad         (filterM)
+import           Control.Monad.ST      (ST, runST)
+import           Data.Array.ST         (STUArray, newArray, newArray_, runSTUArray, writeArray)
+import           Data.Array.Base       (UArray, (!), readArray)
+import           Data.Bool             (bool)
+import qualified Data.ByteString.Char8 as C
+import           Data.Function         ((&))
+import           Data.Functor          ((<&>))
+import           Data.Maybe            (fromJust)
+
+main :: IO ()
+main = do
+    ncsf:ss:fs:ts' <- C.getContents <&> C.lines
+    let [n,c,s,f] = C.words ncsf
+            & map readInt
+        sigma = C.unpack ss
+        ts = map (C.words >>> map readInt >>> zip sigma) ts'
+        table = runSTUArray $ do
+            arr <- newArray_ ((1,'a'),(n,'z'))
+            sequence_ [writeArray arr (i,c) j | (i,ts) <- zip [1..] ts, (c,j) <- ts]
+            pure arr
+        accepting = runSTUArray $ do
+            accept <- newArray (1,n) False
+            C.words fs
+                & map readInt
+                & mapM_ (flip (writeArray accept) True)
+            pure accept
+
+    putStrLn $ runST $ do
+        seen <- newArray (1,n) False
+        bfs seen table accepting ss [s] <&> bool "non-empty" "empty"
+
+bfs :: STUArray s Int Bool -> UArray (Int,Char) Int -> UArray Int Bool -> C.ByteString -> [Int] -> ST s Bool
+bfs seen table accepting sigma [] = pure True
+bfs seen table accepting sigma xs 
+    | any (accepting!) xs = pure False
+    | otherwise = concatMap (\x -> C.foldr (\c -> (table ! (x,c) :)) [] sigma) xs
+                & filterM (\y -> readArray seen y >>= \s -> if s then pure False else writeArray seen y True >> pure True)
+                >>= bfs seen table accepting sigma 
+
+readInt :: C.ByteString -> Int
+readInt = C.readInt >>> fromJust >>> fst
