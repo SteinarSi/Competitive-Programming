a4cf2b1340c467a6d7ed3e8be2c297de440353ee
Steinar <steinar.simonnes@gmail.com>
2025-08-24 19:42:38 +0200

    Baseball Packs

---
+++ Kattis/baseball.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+template<typename Num>
+class Flow {
+    private:
+    int size;
+    int source;
+    int sink;
+    int edge_count = 0;
+    vector<int> to;
+    vector<Num> cap;
+    vector<vector<int>> edge_list;
+    vector<int> parent;
+    vector<int> bfs_q;
+
+    public:
+    vector<bool> seen;
+    Flow(int _source, int _sink, int n){
+        source = _source;
+        sink = _sink;
+        size = n;
+        edge_list = vector<vector<int>>(size);
+    };
+
+    void add_edge(int u, int v, Num weight){
+        to.push_back(v); // add forwards edge
+        cap.push_back(weight);
+        edge_list[u].push_back(edge_count++);
+        to.push_back(u); // add backwards residual edge
+        cap.push_back(0);
+        edge_list[v].push_back(edge_count++);
+    }
+
+    void add_vertex_costs(vector<Num> &costs){
+        edge_list = vector<vector<int>>(size*2);
+        int old_count = edge_count;
+        edge_count = 0;
+        for (int edge {0}; edge < old_count; edge+=2){
+            edge_list[size+to[edge+1]].push_back(edge_count++);
+            edge_list[to[edge]].push_back(edge_count++);
+            to[edge+1] = size+to[edge+1];
+        }
+        for (int u {0}; u < size; u++){
+            add_edge(u, size+u, costs[u]);
+        }
+
+        source += size;
+        size *= 2;
+    }
+
+    Num max_flow_min_cut(){
+        parent = vector<int>(size);
+        seen = vector<bool>(size);
+        bfs_q = vector<int>(edge_count);
+        Num flow {0};
+        do {
+            bfs();
+            if (seen[sink]) flow += update_flow();
+        }
+        while(seen[sink]);
+
+        return flow;
+    }
+
+    private:
+    Num update_flow(){
+        int v = sink;
+        Num bottleneck = cap[parent[v]];
+        while (v != source){
+            bottleneck = min(bottleneck, cap[parent[v]]);
+            v = to[parent[v]^1];
+        }
+        v = sink;
+        while (v != source){
+            cap[parent[v]] -= bottleneck;
+            cap[parent[v]^1] += bottleneck;
+            v = to[parent[v]^1];
+        }
+        return bottleneck;
+    }
+
+    void bfs(){
+        bfs_q[0] = source;
+        int next = 0;
+        int count = 1;
+        seen.assign(size, false);
+        seen[source] = true;
+        while (next < count) {
+            int u = bfs_q[next++];
+            for (int edge : edge_list[u]){
+                if (seen[to[edge]] || cap[edge] <= 0) continue;
+                parent[to[edge]] = edge;
+                bfs_q[count++] = to[edge];
+                seen[to[edge]] = true;
+                if (seen[sink]) break;
+            }
+            if (seen[sink]) break;
+        }
+    }
+};
+
+vector<string> positions = {"1B,","2B,","SS,","3B,","C,","P,","LF,","CF,","RF,","DH,"};
+
+int to_id(string &pos) {
+    for (int i {0}; i < positions.size(); i++) {
+        if (pos == positions[i]) return i;
+    }
+    return -1;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.exceptions(ios::failbit);
+    cout << setprecision(10) << fixed;
+
+    Flow flow = Flow<int>(0,21,22);
+    string n, s;
+
+    for (int i {1}; i <= 10; i++) {
+        cin >> n;
+        flow.add_edge(0,i,1);
+        flow.add_edge(i,20,1);
+        flow.add_edge(i+11,21,1);
+        while ((cin >> s) && (s != "X")) {
+            flow.add_edge(i,11+to_id(s),1);
+        }
+    }
+    int f = flow.max_flow_min_cut();
+    if (f >= 9) cout << "True\n";
+    else cout << "False\n";
+}
