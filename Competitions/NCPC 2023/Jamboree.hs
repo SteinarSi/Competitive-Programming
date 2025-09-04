84bdf8602b0c68a95a75503106db932f93c841b5
Steinar <steinar.simonnes@gmail.com>
2025-09-04 22:39:11 +0200

    Remove some solutions from competitions

--- Competitions/NCPC 2023/Jamboree.hs
+++
@@ -1,7 +0,0 @@
-import           Data.List (sort)
-
-main :: IO ()
-main = do
-    n:m:xs <- fmap (map read . words) getContents
-    let (kids, rem) = splitAt m (replicate (2*m - n) 0 ++ sort xs)
-    print . maximum $ zipWith (+) kids (reverse rem)
--- Competitions/NCPC 2023/Jamboree.py
+++
@@ -1,9 +0,0 @@
-n, m = (int(i) for i in input().split())
-
-items = sorted([int(x) for x in input().split()], reverse=True)
-
-kids = items[:m]
-for i in range(m, n):
-    kids[m-i-1] += items[i]
-
-print(max(kids))
