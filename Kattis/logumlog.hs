7bd79acb911bf3788d6fc2d15d3aa5b369bba767
Steinar <steinar.simonnes@gmail.com>
2025-09-13 19:56:49 +0200

    Lög um lög

---
+++ Kattis/logumlog.hs
@@ -0,0 +1,42 @@
+import           Control.Arrow         ((>>>))
+import qualified Data.ByteString.Char8 as C
+import           Data.Char             (digitToInt)
+import           Data.Functor          ((<&>))
+
+main :: IO ()
+main = do
+    [_,xs,ys] <- C.getContents <&> (C.lines >>> map (C.words >>> map (C.unpack >>> parse)))
+    putStrLn $ if equal xs ys
+        then "Jebb"
+        else "Neibb"
+
+equal :: [Maybe Int] -> [Maybe Int] -> Bool
+equal []            []            = True
+equal (Nothing:xs)  (Nothing:ys)  = equal xs ys
+equal (Just x : xs) (Just y : ys) = map (fmap ((y-x)+)) xs == ys
+equal _             _             = False
+
+parse :: String -> Maybe Int
+parse "-" = Nothing
+parse x = Just $ digitToInt (last x) * 12 + case init x of
+    "Cb" -> -1
+    "C"  -> 0
+    "C#" -> 1
+    "Db" -> 1
+    "D"  -> 2
+    "D#" -> 3
+    "Eb" -> 3
+    "E"  -> 4
+    "Fb" -> 4
+    "E#" -> 5
+    "F"  -> 5
+    "F#" -> 6
+    "Gb" -> 6
+    "G"  -> 7
+    "G#" -> 8
+    "Ab" -> 8
+    "A"  -> 9
+    "A#" -> 10
+    "Bb" -> 10
+    "B"  -> 11
+    "B#" -> 12
